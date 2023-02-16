fce4c676ccf24efb4f8f0894f17ef68599cbfc30
JJ-Bas <jef.j.bas@gmail.com>
2023-02-16 20:59:52 +0100

    waarom staat er bij mij array {pending}???????

--- src/app.js
+++ src/app.js
@@ -1,1 +1,28 @@
-console.log('Hallo daar!');
+import axios, {get} from 'axios'
+
+console.log('Hallo daar!')
+
+async function getCountryInfo() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/name/peru')
+        console.log(result.data)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+getCountryInfo()
+
+async function getCountryList() {
+    try {
+        const result = await axios.get('https://restcountries.com/v2/all?fields=name,flag,population,region')
+        let countryList = result.data
+        countryList.sort((a, b) => { return a.population - b.population})
+        console.log(countryList)
+    } catch
+        (error) {
+        console.error(error)
+    }
+}
+
+getCountryList()
